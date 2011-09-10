0356c2970dbef7de12e9b2d5d418f98161b36c62
Juan Roca <juan.roca@koombea.com>
2011-09-10 13:54:11 -0500

    devise and posts added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilotee::Application.config.secret_token = 'b4d42ed2691e5e3978142ee1b4ed7507a348ac75e723104f560ea632d592c7d49a08120bb44892420fc6ea24016dd920ecbfc5be2008d92be43b656a86c4b2e2'
